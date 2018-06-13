6367faeffbb6c071591f0d3e1a354539214a6d3f
nwelchr <nwelchr@gmail.com>
2018-06-12 23:31:53 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+export const facebook = {
+  clientID: '211426379669572',
+  clientSecret: 'f816d8c5c921d332c86d5dc7211d7d17',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email']
+};
+
+export const google = {
+  clientID:
+    '258527511138-i1odl50suq0hop54rhfo0a3g185i828f.apps.googleusercontent.com',
+  clientSecret: 'MsdiEUP7_e5eyybvj5uB11RF',
+  callbackURL: 'http://localhost:3000/auth/google/callback'
+};
